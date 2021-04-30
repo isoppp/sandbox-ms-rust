87931f41a0ca8e326d7474baefe9cbe1407cb662
isoppp <prog@d2deck.com>
2021-04-30 19:10:28 +0900

    init: Exercise - Fix the code with structs and enums

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -82,6 +82,57 @@ fn basics() {
     let _event: WebEvent = WebEvent::Click{x: 1, y: 1};
 }
 
-fn main() {
+//=====================
+// Exercise
+//=====================
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
 
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    // To Do - Fix this part of the function to create a new Car object as requested by the client
+    let car: Car = todo!("Replace this with an actual Car instance")
+
+    // Factory's Quality Control Department says that new cars must always have zero mileage!
+    assert_eq!(car.mileage, 0);
+
+    // Display the details of the new car order
+    if car.convertible {
+        println!("New car = {}, {:?}, Convertible", car.color, car.transmission);
+    } else {
+        println!("New car = {}, {:?}, Hardtop", car.color, car.transmission);
+    }
+
+    return car;
+}
+
+fn car_factory_assert() {
+    let client_request_1 = car_factory(String::from("Red"), Transmission::Manual, false);
+    assert_eq!(client_request_1.color, "Red");
+    assert_eq!(client_request_1.transmission, Transmission::Manual);
+    assert_eq!(client_request_1.convertible, false);
+
+    let client_request_2 = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    assert_eq!(client_request_2.color, "Silver");
+    assert_eq!(client_request_2.transmission, Transmission::Automatic);
+    assert_eq!(client_request_2.convertible, true);
+
+    let client_request_2 = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    assert_eq!(client_request_2.color, "Yellow");
+    assert_eq!(client_request_2.transmission, Transmission::SemiAuto);
+    assert_eq!(client_request_2.convertible, false);
+}
+
+fn main() {
+    car_factory_assert();
 }
